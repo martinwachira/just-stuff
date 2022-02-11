f2d421d38ef541c85de86df7750380021c6a0ab6
martin-wachira <martinnwachira@gmail.com>
2022-02-11 15:02:23 +0300

    add some more

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // Given a string s="abcabcbb", find the length of the longest substring without repeating characters.
 
 const lengthOfLongestSubstring = (s) => {
@@ -30,6 +29,8 @@ const lengthOfLongestSubstring = (s) => {
 /**
     Implementation of Null Object Pattern
 */
+
+/** ------------------------------------------------------------------------ */
 
 class User {
   constructor(id, name, role) {
@@ -91,6 +92,8 @@ printUser(3);
 printUser(4);
 printUser(5);
 printUser(99);
+
+/** ------------------------------------------------------------------------ */
 
 /**
  * Implementation of Builder Pattern
@@ -154,3 +157,65 @@ const person = new Person("Martin", {
   address: new Address("00100", "Nairobi"),
 });
 console.log(person);
+
+/** ------------------------------------------------------------------------ */
+
+/**
+ * Grading method
+ */
+const grades = [73, 67, 38, 33];
+
+function gradding(grades) {
+  // Write your code here
+  for (var i = 0; i < grades.length; i++) {
+    let m = 0;
+    if (grades[i] >= 38) {
+      m = Math.ceil(grades[i] / 5) * 5;
+      if (m - grades[i] < 3) {
+        console.log("Pass", (grades[i] = m));
+      } else {
+        console.log("Pass", grades[i]);
+      }
+    } else {
+      console.log("Fail", grades[i]);
+    }
+  }
+  return grades;
+}
+
+gradding(grades);
+
+/** ------------------------------------------------------------------------ */
+
+/**
+ * There is a large pile of socks that must be paired by color.
+ * Given an array of integers representing the color of each sock,
+ * determine how many pairs of socks with matching colors there are.
+ */
+
+const ar = [1, 2, 3, 4, 2, 3, 2, 2, 2, 1, 5, 6, 7, 5, 6, 7];
+const n = 9;
+function sockMerchant(n, ar) {
+  let x = 0;
+  let counts = [];
+  let newAr = [];
+
+  for (let i = 0; i < ar.length; i++) {
+    x = ar[i];
+    counts[x] = counts[x] >= 1 ? counts[x] + 1 : 1;
+    //     if (counts[x] >= 1) {
+    //       counts[x] = counts[x] + 1;
+    //     } else {
+    //       counts[x] = 1;
+    //     }
+    if (counts[x] % 2 === 0) {
+      newAr.push(x);
+    }
+  }
+  console.log("newAr =", newAr, "-- length of newAr =", newAr.length);
+  return newAr.length;
+}
+
+sockMerchant(n, ar);
+
+/** ------------------------------------------------------------------------ */
